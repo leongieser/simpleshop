3bf27305abd5940feddf7387028ccf670f990442
Leon Gieser <leon.gieser@gmail.com>
2023-07-25 16:12:20 +0200

    fix/cartQuantityDot dimensions

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,32 +1,20 @@
-
-
 import Image from "next/image";
-
 
 import { useCartStore } from "@/app/(store)/cart";
 
 export default function CartButton({ handleCartClick }: { handleCartClick: () => void }) {
-
   const { cart } = useCartStore();
 
   const totalQuantity = cart.reduce((acc, curr) => acc + curr.quantity!, 0);
 
-  const quantitiyDotDimensionsMap = () => {
-    if (totalQuantity < 10) return 7;
-    if (totalQuantity < 100) return 8;
-    if (totalQuantity < 1000) return 12;
-  };
+  const quantityDotWidth = totalQuantity > 100 ? "w-8" : totalQuantity > 10 ? "w-7" : "w-5";
+  const quantityDotHeight = totalQuantity > 100 ? "h-8" : totalQuantity > 10 ? "h-7" : "h-5";
 
   return (
     <>
-      <button className="relative" onClick={handleCartClick}>
+      <button className="relative bg-red-300" onClick={handleCartClick}>
         {cart.length > 0 && (
-          <div
-            className={`absolute flex items-center justify-center
-          w-${quantitiyDotDimensionsMap()}
-          h-${quantitiyDotDimensionsMap()}
-          p-1 m-0 text-sm text-white rounded-full -right-1 -top-3 bg-emerald-600`}
-          >
+          <div className={`absolute flex items-center justify-center flex-grow ${quantityDotWidth} ${quantityDotHeight}  text-white rounded-full -right-1 -top-3 bg-emerald-600`}>
             {totalQuantity}
           </div>
         )}
@@ -38,7 +26,6 @@ export default function CartButton({ handleCartClick }: { handleCartClick: () =>
           className={`h-auto mr-2`}
         ></Image>
       </button>
-
     </>
   );
 }
